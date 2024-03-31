80c2b025294aae59d2033617218b3be878f8f3a7
Khan Kibria <khan.kibria@gmail.com>
2024-03-31 02:09:12 -0700

    update

--- template/prj/src/{{__target__}}/cli.py
+++ template/prj/src/{{__target__}}/cli.py
@@ -6,11 +6,11 @@ from . import get_template_path, set_warnigs_hook
 from .hook import MyGen
 
 
-def build(dstpath, params):
-    dst = Path(dstpath)
-    if not params["force"]:
+def build(params, path, force, **kwargs):
+    dst = Path(path)
+    if not force:
         if dst.exists():
-            warn(f'"{dstpath}" already exists, use --force, exiting!')
+            warn(f'"{path}" already exists, use --force, exiting!')
             return
     g = MyGen(get_template_path())
     g.update_params(params)
@@ -30,11 +30,11 @@ def main():
                     action='store_true')
     
     args = parser.parse_args()
-    params["force"] = args.force
+    params = params | vars(args)
 
     set_warnigs_hook()
     try:
-        build(args.path, params)
+        build(params, **params)
     except Exception as e:
         print(f'{e.__class__.__name__}:', *e.args)
         return 1
